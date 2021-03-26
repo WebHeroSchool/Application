afbb19daa1e4794c87ca8203be1cbdc8f451e353
Antoha208 <anton_androsyak@mail.ru>
2021-03-26 06:35:08 +0300

    nineth

--- todo/src/components/ItemList/ItemList.js
+++ todo/src/components/ItemList/ItemList.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import Item from '../Item/Item';
 
-const ItemList = ({ todoItem }) => (<ul>
-    <li><Item todoItem={ todoItem } /></li>
-    <li><Item todoItem={ 'Прописать README' } /></li>
-    <li><Item todoItem={ 'Пофиксить баги' } /></li>
-    <li><Item todoItem={ 'Сдать проект' } /></li>
-  </ul>);
+const ItemList = ({ items }) => (<ul>
+    {items.map(item => <li key={item.value}><Item value={ item.value } /></li>)}
+</ul>);
 
-  export default ItemList; 
+ export default ItemList; 

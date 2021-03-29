9396f750cc2197528e2dacf2240273c18944a741
Antoha208 <anton_androsyak@mail.ru>
2021-03-29 20:47:48 +0300

    state

--- todo/src/components/App/App.js
+++ todo/src/components/App/App.js
@@ -6,34 +6,36 @@ import styles from './App.module.css';
 
 
 class App extends React.Component {
+  state = {
+      items: [
+          {
+              value: 'Написать приложение',
+              isDone: true
+          },
+          {
+              value: 'Прописать README',
+              isDone: false
+          },
+          {
+              value: 'Пофиксить баги',
+              isDone: true
+          },
+          {
+              value: 'Сдать проект',
+              isDone: false
+          }
+      ]
+  };
+
   render() {
-    const items = [
-      {
-      value: 'Написать приложение',
-      isDone: true
-      },
-      {
-      value: 'Прописать README',
-      isDone: false
-      },
-      {
-      value: 'Пофиксить баги',
-      isDone: true
-      },
-      {
-      value: 'Сдать проект',
-      isDone: false
-      }
-    ];
-    
     return (
-    <div className = { styles.wrap }>
-      <h1 className = { styles.title }>TODOLIST</h1>
-      <InputItem />
-      <ItemList items = { items } />
-      <Footer count = {2} />
-    </div>);
-  }
+      <div className = { styles.wrap }>
+        <h1 className = { styles.title }>TODOLIST</h1>
+        <InputItem />
+        <ItemList items = { this.state.items } />
+        <Footer count = {2} />
+      </div>);
+    }
 };
 
 export default App; 

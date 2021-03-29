6943124916af64ae7dba4dd65a5ac85e9d82e082
Antoha208 <anton_androsyak@mail.ru>
2021-03-29 05:48:23 +0300

    stylize

--- todo/src/components/InputItem/InputItem.js
+++ todo/src/components/InputItem/InputItem.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
+import styles from './InputItem.module.css';
 
-const InputItem = () => (<div>
+const InputItem = () => (<div className = { styles.input }>
     <TextField 
     id="standard-helperText"
-    label="Найти задание"
+    style={{ margin: 8 }}
+          placeholder="Add new task"
+          fullWidth
+          margin="normal"
+          InputLabelProps={{
+            shrink: true,
+          }}
+    label="What would you want to add?"
     />
 </div>)
 
 export default InputItem; 
+
+          

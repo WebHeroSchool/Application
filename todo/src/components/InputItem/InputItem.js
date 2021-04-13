5c3b7eeed945415f802548ecbe0b6e80b7781f42
Antoha208 <anton_androsyak@mail.ru>
2021-04-13 06:33:47 +0300

    input2

--- todo/src/components/InputItem/InputItem.js
+++ todo/src/components/InputItem/InputItem.js
@@ -2,8 +2,43 @@ import React from 'react';
 import TextField from '@material-ui/core/TextField';
 import styles from './InputItem.module.css';
 import Button from '@material-ui/core/Button';
+import Alert from '@material-ui/lab/Alert';
 
-const InputItem = () => (<div className = { styles.input }>
+
+class InputItem extends React.Component {
+  state = {
+    InputValue: ''
+  };
+
+  onButtonClick = () => {
+    this.setState ({
+      InputValue: ''
+    });
+
+    this.props.onClickAdd(this.state.InputValue);
+  }
+
+  render() {
+    const { onClickAdd } = this.props;
+
+    const error = this.props.isError;
+    let alert;
+
+    if (error) {
+      alert = <Alert 
+      severity="error">This field cannot be empty!
+      </Alert>
+    } else {
+			alert = ''
+		}
+
+    return (<div className = { styles.input }>
+    
+    <div>
+      { alert }
+    </div>
+   
+
     <TextField 
     id="standard-helperText"
     style={{ margin: 8 }}
@@ -14,11 +49,21 @@ const InputItem = () => (<div className = { styles.input }>
             shrink: true,
           }}
     label="What would you want to add?"
+    value = { this.state.InputValue }
+    onChange = {event => this.setState({ InputValue: event.target.value.toUpperCase() })}
     />
-    <Button variant="contained" color="primary" className = { styles.create }>
-      Create
+
+    <Button 
+      variant="contained" 
+      color="primary" 
+      className = { styles.create }
+      onClick = { this.onButtonClick } 
+    >
+      Create 
     </Button>
-</div>)
+</div>);
+  }
+}
 
 export default InputItem; 
 
